509c9124ca9f6ec07ba3acf3e7c496625418e045
Joseph R <raczynski.joseph616@gmail.com>
2026-02-21 16:34:56 -0600

    Add AboutPage

---
+++ axiomata-frontend/src/pages/AboutPage.jsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About Axiomata</h1>
+            <p>Axiomata is a world-building platform designed to help you create, explore, and manage rich fictional worlds.</p>
+            <p>This page is publicly accessible and serves as an introduction to the project.</p>
+        </div>
+    )
+}
