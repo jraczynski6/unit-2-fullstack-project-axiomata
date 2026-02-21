1fbaae3ff7367841736108263835ee71f9627a10
Joseph R <raczynski.joseph616@gmail.com>
2026-02-21 13:29:20 -0600

    Update RegisterForm for handleSubmit

--- axiomata-frontend/src/main.jsx
+++ axiomata-frontend/src/main.jsx
@@ -1,7 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css';
+import App from './App.jsx';
 import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById('root')).render(
