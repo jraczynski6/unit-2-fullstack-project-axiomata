424e32107e3f4f542f68ae82851fee49ea3b83e5
Joseph R <raczynski.joseph616@gmail.com>
2026-02-21 13:19:13 -0600

    Update LoginForm with handleSubmit

--- axiomata-frontend/src/pages/LoginForm.jsx
+++ axiomata-frontend/src/pages/LoginForm.jsx
@@ -1,8 +1,44 @@
-export default function LoginForm() {
-    return (
-    <div>
-      <h2>Login</h2>
-      {/* Add form input */}
-    </div>
-  )
+import { useState } from "react";
+import api from "../services/api";
+import { useAuth } from "../context/AuthContext";
+
+export default function LoginForm({ onSucess }) {
+  const { login } = useAuth();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.post("/auth/login", { username, password });
+      const { token } = response.data;
+
+      login(token);
+    } catch (err) {
+      setError(err.response?.data?.message || "Login failed");
+    }
+  };
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button type="submit">Login</button>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </form>
+  );
 }
