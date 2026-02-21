b654fae0f0eba9c0d5567d08238bad0c7c0925ed
Joseph R <raczynski.joseph616@gmail.com>
2026-02-21 12:00:48 -0600

    Add Route structure and ProtectedRoute

--- axiomata-frontend/src/App.jsx
+++ axiomata-frontend/src/App.jsx
@@ -1,8 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import LandingPage from "./pages/LandingPage"
+import AuthPage from "./pages/AuthPage"
+import DashboardPage from "./pages/Dashboard"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+
 function App() {
   return (
-    <div>
-      <h1>Axiomata</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Public Pages */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+
+        {/* Protected Route */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
