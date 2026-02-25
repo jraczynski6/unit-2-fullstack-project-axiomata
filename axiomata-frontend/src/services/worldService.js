fc9852b10d03dca6d8436e27856f173633468191
Joseph R <raczynski.joseph616@gmail.com>
2026-02-25 09:07:46 -0600

    Create worldService.js

--- axiomata-frontend/src/services/worldService.js
+++ axiomata-frontend/src/services/worldService.js
@@ -1,10 +1,11 @@
-import api from "/.api";
+import api from "./api";
 
+// worldService only speaks with back end
 export const getWorldsForUser = async () => {
     const response = await api.get("/worlds");
     return response.data;
 }
 
-export const getWorldById = async () => {
+export const getWorldById = async (worldId) => {
     const response = await api.get(`/worlds/${worldId}`);
 }
